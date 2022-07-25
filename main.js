1d0c8490b793160f34dc8637f58a54c336b7f935
Jordan Phillips <j.phillips.git@gmail.com>
2022-07-25 04:50:40 -0600

    start theme setup

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const root = document.documentElement;
+const theme = 'theme';
+const dataTheme = 'data-theme';
+const themeSwitcherDark = document.querySelector('.theme__toggle--dark');
+const themeSwitcherLight = document.querySelector('.theme__toggle--light');
+const light = 'light';
+const dark = 'dark';
+
+// Theme 
+const currentTheme = localStorage.getItem(theme);
