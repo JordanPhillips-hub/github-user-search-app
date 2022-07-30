3fb1ebf282fb49b7664f8fb9e70eda51a3bf953d
Jordan Phillips <j.phillips.git@gmail.com>
2022-07-30 10:54:39 -0600

    set dark / light theme

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
 'use strict';
 
-const root = document.documentElement;
+// Theme
 const theme = 'theme';
 const dataTheme = 'data-theme';
-const themeSwitcherDark = document.getElementById('themeDark');
-const themeSwitcherLight = document.getElementById('themeLight');
-const search = document.getElementById('search');
+const root = document.documentElement;
+const toggleDark = document.getElementById('toggleDark');
+const toggleLight = document.getElementById('toggleLight');
+const dark = 'dark';
+const light = 'light';
+const currentTheme = localStorage.getItem(theme);
 const icons = document.querySelectorAll('.links__inner__content__img');
 
-// Theme 
-const currentTheme = localStorage.getItem(theme);
-
-// Switch to dark theme
-const toggleDark = () => {
-  root.classList.add('dark');
-  themeSwitcherDark.classList.add('hidden');
-  themeSwitcherLight.classList.remove('hidden');
-  search.style.backgroundColor = 'hsl(222, 41%, 20%)';
+const setDark = () => {
+  toggleDark.classList.add('hidden');
+  toggleLight.classList.remove('hidden');
   icons.forEach(icon => {
-    icon.classList.add('icon-light');
+    icon.classList.add('icon--light');
   });
-}
+};
 
-// Switch to light theme
-const toggleLight = () => {
-  root.classList.remove('dark');
-  search.style.backgroundColor = 'hsl(0, 0%, 100%)';
-  themeSwitcherDark.classList.remove('hidden');
-  themeSwitcherLight.classList.add('hidden');
+const setLight = () => {
+  toggleLight.classList.add('hidden');
+  toggleDark.classList.remove('hidden');
   icons.forEach(icon => {
-    icon.classList.remove('icon-light');
+    icon.classList.remove('icon--light');
   });
-}
+};
 
-themeSwitcherDark.addEventListener('click', toggleDark);
-themeSwitcherLight.addEventListener('click', toggleLight);
+toggleDark.addEventListener('click', function () {
+  root.setAttribute(dataTheme, dark);
+  localStorage.setItem(theme, dark);
+  setDark();
+});
+
+toggleLight.addEventListener('click', function () {
+  root.setAttribute(dataTheme, light);
+  localStorage.setItem(theme, light);
+  setLight();
+});
+
+// Set current theme on page load
+if (currentTheme) {
+  root.setAttribute(dataTheme, currentTheme);
+  if (root.dataset.theme.includes(dark)) {
+    setDark();
+  };
+};
